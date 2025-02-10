e924e90b67e931116b70cc52e56630b3240e2794
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-10 17:12:32 +0100

    notes successfully tested

---
+++ tests/ProofTest.spec.ts
@@ -0,0 +1,35 @@
+
+import { deposit, generateNoteWithdrawProof, parseNote, verifyThreePublicSignals } from "../lib/notes";
+import fs from "fs";
+import assert from "assert";
+import { Address } from "@ton/core";
+
+const SplitAddress = (addrString: string) => {
+    return addrString.split(":")[1];
+}
+
+const hexToBigint = (hex: string) => {
+    return BigInt("0x" + hex);
+}
+
+describe("Proof test", () => {
+
+    it("should create a note, a proof to withdraw and verify it", async () => {
+        const noteString = await deposit({ currency: "tbtc", amount: 10 });
+        const parsedNote = await parseNote(noteString);
+
+        const recipient_address = Address.parse("UQB9_eAKXGpTlx9I8qrkSjHMiDomWTrv6G7fBBb5Wj10_v-v");
+
+        const splitRaw = SplitAddress(recipient_address.toRawString());
+
+        const recipient_bigint = hexToBigint(splitRaw);
+
+        const { proof, publicSignals } = await generateNoteWithdrawProof({ deposit: parsedNote.deposit, recipient: recipient_bigint, snarkArtifacts: undefined })
+        const verificationKeyFile = fs.readFileSync("circuits/verification_key.json", "utf-8");
+        const verificationKey = JSON.parse(verificationKeyFile);
+        const res = await verifyThreePublicSignals(verificationKey, { proof, publicSignals });
+        assert.equal(res, true);
+
+    })
+
+})
