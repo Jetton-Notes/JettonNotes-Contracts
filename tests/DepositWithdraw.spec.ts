5536c740e73fac7887f092b158924469a229deb1
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-10 18:23:56 +0100

    zkp verification passes

--- tests/DepositWithdraw.spec.ts
+++ tests/DepositWithdraw.spec.ts
@@ -1,10 +1,16 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Cell, toNano } from '@ton/core';
+import { Address, Cell, toNano } from '@ton/core';
 import { DepositWithdraw } from '../wrappers/DepositWithdraw';
 import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
+import { deposit, g1Compressed, g2Compressed, generateNoteWithdrawProof, hexToBigint, parseNote, SplitAddress } from '../lib/notes';
 
-//TODO: implement the tests here
+
+
+//@ts-ignore
+import { buildBls12381, utils } from "ffjavascript";
+const { unstringifyBigInts } = utils;
+
 
 
 describe('DepositWithdraw', () => {
@@ -43,10 +49,47 @@ describe('DepositWithdraw', () => {
         });
     });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and depositWithdraw are ready to use
-    });
+    // it('should deploy', async () => {
+    //     // the check is done inside beforeEach
+    //     // blockchain and depositWithdraw are ready to use
+    // });
+
+    it("should verify", async () => {
+        const noteString = await deposit({ currency: "tbtc", amount: 1 });
+        const parsedNote = await parseNote(noteString);
+        const recipient_address = deployer.address;
+        const splitRaw = SplitAddress(recipient_address.toRawString())
+        const recipient_bigint = hexToBigint(splitRaw);
+        const { proof, publicSignals } = await generateNoteWithdrawProof({ deposit: parsedNote.deposit, recipient: recipient_bigint, snarkArtifacts: undefined })
+        const curve = await buildBls12381();
+        const proofProc = unstringifyBigInts(proof);
+        const pi_aS = g1Compressed(curve, proofProc.pi_a);
+        const pi_bS = g2Compressed(curve, proofProc.pi_b);
+        const pi_cS = g1Compressed(curve, proofProc.pi_c);
+        const pi_a = Buffer.from(pi_aS, "hex");
+        const pi_b = Buffer.from(pi_bS, "hex");
+        const pi_c = Buffer.from(pi_cS, "hex");
+
+        const verifier = await blockchain.treasury("verifier");
+        console.log(publicSignals)
+        const verifyResult = await depositWithdraw.sendWithdraw(
+            verifier.getSender(),
+            {
+                pi_a: pi_a,
+                pi_b: pi_b,
+                pi_c: pi_c,
+                pubInputs: publicSignals,
+                value: toNano("0.15") //0.15 TON fee
+            });
+        expect(verifyResult.transactions).toHaveTransaction({
+            from: verifier.address,
+            to: depositWithdraw.address,
+            success: true
+        })
+
+        //TODO: Can do fetching with public view function to check a result
+
+    })
 
     // it('should increase counter', async () => {
     //     const increaseTimes = 3;
