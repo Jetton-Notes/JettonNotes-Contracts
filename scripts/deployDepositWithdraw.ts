d618fe67fcc6e10e443c8e2c57fa59cf864ab048
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-15 22:28:23 +0100

    deploy script updated

--- scripts/deployDepositWithdraw.ts
+++ scripts/deployDepositWithdraw.ts
@@ -1,13 +1,24 @@
-import { toNano } from '@ton/core';
+import { Address, toNano } from '@ton/core';
 import { DepositWithdraw } from '../wrappers/DepositWithdraw';
 import { compile, NetworkProvider } from '@ton/blueprint';
 
+//TODO: Deploy on testnet
+
+//TODO:
+
+const JETTON_MASTER_ADDRESS = ""
+
 export async function run(provider: NetworkProvider) {
+    const sender = provider.sender()
+    const address = sender.address as Address;
+    const jetton_master_address = Address.parse(JETTON_MASTER_ADDRESS)
     const depositWithdraw = provider.open(
         DepositWithdraw.createFromConfig(
             {
-                id: Math.floor(Math.random() * 10000),
-                counter: 0,
+                init: 0,
+                jetton_wallet_address: jetton_master_address,
+                jetton_wallet_set: 0,
+                creator_address: address
             },
             await compile('DepositWithdraw')
         )
@@ -17,5 +28,8 @@ export async function run(provider: NetworkProvider) {
 
     await provider.waitForDeploy(depositWithdraw.address);
 
-    console.log('ID', await depositWithdraw.getID());
+    console.log("Contract address: ", depositWithdraw.address);
+
+    // console.log('ID', await depositWithdraw.getID());
+    //TODO: Console log the contract address
 }
