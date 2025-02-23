2b7aa6b8e86a38fab9fd6e2a5fe379cae6916768
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-23 22:33:13 +0100

    added a test for key derivation via @ton/crypto

--- tests/HDwallet.spec.ts
+++ tests/HDwallet.spec.ts
@@ -1,5 +1,7 @@
 import { bip32derivedDeposit } from "../lib/cryptonotes";
-
+import { mnemonicToHDSeed, deriveEd25519Path, KeyPair, keyPairFromSeed } from '@ton/crypto';
+//@ts-ignore
+import { utils } from "ffjavascript";
 
 describe("secret derivation via hd wallet bip 32", () => {
     it("should create an hd wallet", async () => {
@@ -17,6 +19,19 @@ describe("secret derivation via hd wallet bip 32", () => {
         expect(deposit2.nullifier).toBe(89686447295344543224647122901103962685186689836187504686823726079571009777441n)
 
         // ..etc.. The keys can be further derived from the previous values
+    })
+
+    it("should create an hd wallet with @ton/crypto", async () => {
+        const firstSecret = 94689101999558503447711315587047797285553390663767543862203419261732524810301n;
+        const firstNullifier = 72019624781118465516028022228456025484404980050365267869270564861953969640268n;
+
+        const derivedMasterSeed = await deriveEd25519Path(utils.leInt2Buff(firstSecret), [0, 1, 1, 1]);
+        const keyPair: KeyPair = keyPairFromSeed(derivedMasterSeed);
+
+        console.log(keyPair.secretKey.length);
+        console.log(keyPair.publicKey.length);
+
+        //TODO: I could hash the secretkey to reduce the size to 32 bits, with sha256...
     })
 
 })
